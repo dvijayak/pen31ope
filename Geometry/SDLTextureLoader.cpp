8e1c07a4f0fbf96df7f8e7b663e9e4d2ccddbdcd
Daniel Vijayakumar <daniel.vijayakumar@mappedin.ca>
2020-03-15 23:30:01 -0400

    PoC: Rectified algorithm used to read pixels from a texture image; need to take into account bytes per pixel

--- Geometry/SDLTextureLoader.cpp
+++ Geometry/SDLTextureLoader.cpp
@@ -1,8 +1,43 @@
 #include "SDLTextureLoader.hpp"
 
+#include <cassert>
+
 #include "SDL.h"
 #include "SDL_image.h"
 #include "Color.hpp"
+
+// Courtesy of http://sdl.beuc.net/sdl.wiki/Pixel_Access
+Uint32 getpixel (SDL_Surface *surface, int i)
+{
+    int bpp = surface->format->BytesPerPixel;
+    /* Here p is the address to the pixel we want to retrieve */
+    Uint8 *p = (Uint8 *)surface->pixels + i * bpp;
+
+    switch(bpp) {
+    case 1:
+        return *p;
+        break;
+
+    case 2:
+        return *(Uint16 *)p;
+        break;
+
+    case 3:
+        if(SDL_BYTEORDER == SDL_BIG_ENDIAN)
+            return p[0] << 16 | p[1] << 8 | p[2];
+        else
+            return p[0] | p[1] << 8 | p[2] << 16;
+        break;
+
+    case 4:
+        return *(Uint32 *)p;
+        break;
+
+    default:
+        assert(false);
+        return 0;       /* shouldn't happen, but avoids warnings */
+    }
+}
 
 std::unique_ptr<TextureMap> SDLTextureLoader::LoadFromFile (std::string fileName)
 {
@@ -22,7 +57,7 @@ std::unique_ptr<TextureMap> SDLTextureLoader::LoadFromFile (std::string fileName
       auto pixels = TextureMap::PixelBuffer(totalPixels);
       for (size_t i = 0; i < totalPixels; ++i)
       {
-         Uint32 color = reinterpret_cast<Uint32*>(pTextureImage->pixels)[i];
+         Uint32 color = getpixel(pTextureImage, i);
          Uint8 r, g, b;
          SDL_GetRGB(color, pTextureImage->format, &r, &g, &b);
          ColorRGB finalColor = Color::Mix(r, g, b);
